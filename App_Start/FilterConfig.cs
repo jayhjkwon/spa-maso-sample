e7356cc320b1215716a834872b21cb8281d634ce
Ray H.J Kwon <nicekhj@gmail.com>
2012-12-03 08:58:36 +0900

    first commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace spa_maso_sample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
